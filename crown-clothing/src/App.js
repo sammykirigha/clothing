61f6667c14bbe01bf7a15b5f12e7620359eaf7f9
sammy kirigha <dkirigha18@gmail.com>
2020-06-16 10:29:41 +0300

    intergrated firebase into application, created sign in page with sign in components

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -1,22 +1,48 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
-import './App.css';
+import './App.scss';
 import HomePage from './pages/homepage/HomePage.componenet';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header';
+import SignInSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up';
+import {auth} from './firebase/firebase.utils';
 
 
 
-function App() {
-  return (
-    <div>
-      <Header/>
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-       <Route exact path='/shop' component={ShopPage} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      currentUser: null
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount(){
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user})
+    })
+  }
+
+  componentWillUnmount(){
+    this.unsubscribeFromAuth()
+  }
+
+  render (){
+    return (
+      <div>
+        <Header currentUser = {this.state.currentUser}/>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route  path='/shop' component={ShopPage} />
+          <Route  path='/signin' component={SignInSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
+ 
 }
 
 export default App;
---
+++ crown-clothing/src/pages/sign-in-sign-up/sign-in-sign-up.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './sign-in-sign-up.scss'
+import SignIn from '../../components/sign-in/sign-in';
+
+const SignInSignUpPage = () => (
+    <div className="sign-in-sign-up">
+        <SignIn/>
+        </div>
+)
+
+export default SignInSignUpPage;
